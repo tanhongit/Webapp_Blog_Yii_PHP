1b25bc2f4be3755651bd3bb02b5f709a454a31f7
tanhongit <phuongtan12357nguyen@gmail.com>
2021-05-14 11:43:06 +0700

    generate post & comment

---
+++ blog/protected/views/post/_form.php
@@ -0,0 +1,66 @@
+<?php
+/* @var $this PostController */
+/* @var $model Post */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'post-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'title'); ?>
+		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'title'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'content'); ?>
+		<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'content'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'tag'); ?>
+		<?php echo $form->textField($model,'tag',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'tag'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'status'); ?>
+		<?php echo $form->textField($model,'status'); ?>
+		<?php echo $form->error($model,'status'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'create_time'); ?>
+		<?php echo $form->textField($model,'create_time'); ?>
+		<?php echo $form->error($model,'create_time'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'update_time'); ?>
+		<?php echo $form->textField($model,'update_time'); ?>
+		<?php echo $form->error($model,'update_time'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'author_id'); ?>
+		<?php echo $form->textField($model,'author_id'); ?>
+		<?php echo $form->error($model,'author_id'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
