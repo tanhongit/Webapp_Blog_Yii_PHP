c682907327c04742939899127655548611365269
tanhongit <phuongtan12357nguyen@gmail.com>
2021-05-18 15:17:04 +0700

    fix gii module does not allow access

--- blog/protected/config/main.php
+++ blog/protected/config/main.php
@@ -34,7 +34,7 @@ return array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>false,
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'ipFilters'=>array('127.0.0.1','::1','*'),
 		),
 	),
 
