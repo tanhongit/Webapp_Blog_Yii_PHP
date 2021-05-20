3bd94df0e9fb7722accfd3791fc304aa2a7151f2
tanhongit <phuongtan12357nguyen@gmail.com>
2021-05-20 15:32:49 +0700

    set datetime by internationalization & localization

---
+++ blog/protected/messages/en/app.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+'model.post.title' => 'Title en'
+, 'model.post.content' => 'Content en'
+, 'model.post.tag'=> 'Tag'
+, 'model.post.status'=> 'Status en'
+, 'model.post.create_time'=> 'Create date en'
+, 'model.post.update_time'=> 'Update date en'
+, 'model.post.author'=> 'Author en'
+
+, 'model.tag.name'=> 'Name'
+, 'model.tag.frequency'=> 'Tần số'
+);
