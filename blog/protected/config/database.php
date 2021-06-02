7fa15e1a4f83f22ba804e84d6893ce2d6c9fb581
tanhongit <phuongtan12357nguyen@gmail.com>
2021-06-02 09:26:23 +0700

    update note

--- blog/protected/config/database.php
+++ blog/protected/config/database.php
@@ -3,7 +3,7 @@ return array(
     'connectionString' => 'mysql:host=localhost;dbname=blogbasic',
     'emulatePrepare' => true,
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
     'tablePrefix' => 'tbl_',
 );
