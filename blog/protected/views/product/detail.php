794416cfae85c059d34eafee819af487d23ac382
tanhongit <phuongtan12357nguyen@gmail.com>
2021-06-11 22:58:10 +0700

    edit star rating for the product

--- blog/protected/views/product/detail.php
+++ blog/protected/views/product/detail.php
@@ -5,430 +5,9 @@ $this->breadcrumbs = array(
     'Detail'
 );
 ?>
-<style>
-    /* review */
-    /**
- * Oscuro: #283035
- * Azul: #03658c
- * Detalle: #c7cacb
- * Fondo: #dee1e3
- ----------------------------------*/
-    * {
-        margin: 0;
-        padding: 0;
-        -webkit-box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-    }
 
-    ul {
-        list-style-type: none;
-    }
+<link rel="stylesheet" href="<?= get_BaseUrl(); ?>/css/review-product.css">
 
-    body {
-        font-family: 'Roboto', Arial, Helvetica, Sans-serif, Verdana;
-    }
-
-    * {
-        -webkit-box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-    }
-
-    *:before,
-    *:after {
-        -webkit-box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-    }
-
-    .clearfix {
-        clear: both;
-    }
-
-    .text-center {
-        text-align: center;
-    }
-
-    a {
-        color: tomato;
-        text-decoration: none;
-    }
-
-    a:hover {
-        color: #2196f3;
-    }
-
-    pre {
-        display: block;
-        padding: 9.5px;
-        margin: 0 0 10px;
-        font-size: 13px;
-        line-height: 1.42857143;
-        color: #333;
-        word-break: break-all;
-        word-wrap: break-word;
-        background-color: #F5F5F5;
-        border: 1px solid #CCC;
-        border-radius: 4px;
-    }
-
-    .header {
-        padding: 20px 0;
-        position: relative;
-        margin-bottom: 10px;
-
-    }
-
-    .header:after {
-        content: "";
-        display: block;
-        height: 1px;
-        background: #eee;
-        position: absolute;
-        left: 30%;
-        right: 30%;
-    }
-
-    .header h2 {
-        font-size: 3em;
-        font-weight: 300;
-        margin-bottom: 0.2em;
-    }
-
-    .header p {
-        font-size: 14px;
-    }
-
-
-
-    #a-footer {
-        margin: 20px 0;
-    }
-
-    .new-react-version {
-        padding: 20px 20px;
-        border: 1px solid #eee;
-        border-radius: 20px;
-        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-
-        text-align: center;
-        font-size: 14px;
-        line-height: 1.7;
-    }
-
-    .new-react-version .react-svg-logo {
-        text-align: center;
-        max-width: 60px;
-        margin: 20px auto;
-        margin-top: 0;
-    }
-
-    .success-box {
-        margin: 50px 0;
-        padding: 10px 10px;
-        border: 1px solid #eee;
-        background: #f9f9f9;
-    }
-
-    .success-box img {
-        margin-right: 10px;
-        display: inline-block;
-        vertical-align: top;
-    }
-
-    .success-box>div {
-        vertical-align: top;
-        display: inline-block;
-        color: #888;
-    }
-
-
-
-    /* Rating Star Widgets Style */
-    .rating-stars ul {
-        list-style-type: none;
-        padding: 0;
-
-        -moz-user-select: none;
-        -webkit-user-select: none;
-    }
-
-    .rating-stars ul>li.star {
-        display: inline-block;
-
-    }
-
-    /* Idle State of the stars */
-    .rating-stars ul>li.star>i.fa {
-        font-size: 2.5em;
-        /* Change the size of the stars */
-        color: #ccc;
-        /* Color on idle state */
-    }
-
-    /* Hover state of the stars */
-    .rating-stars ul>li.star.hover>i.fa {
-        color: #FFCC36;
-    }
-
-    /* Selected state of the stars */
-    .rating-stars ul>li.star.selected>i.fa {
-        color: #FF912C;
-    }
-
-
-    /** ====================
-* Lista de Comentarios
-=======================*/
-    .comments-container {
-        margin: 60px auto 15px;
-        width: 768px;
-    }
-
-    .comments-container h1 {
-        font-size: 36px;
-        color: #283035;
-        font-weight: 400;
-    }
-
-    .comments-container h1 a {
-        font-size: 18px;
-        font-weight: 700;
-    }
-
-    .comments-list {
-        margin-top: 30px;
-        position: relative;
-    }
-
-    /**
-* Lineas / Detalles
------------------------*/
-    .comments-list:before {
-        content: '';
-        width: 2px;
-        height: 100%;
-        background: #c7cacb;
-        position: absolute;
-        left: 32px;
-        top: 0;
-    }
-
-    .comments-list:after {
-        content: '';
-        position: absolute;
-        background: #c7cacb;
-        bottom: 0;
-        left: 27px;
-        width: 7px;
-        height: 7px;
-        border: 3px solid #dee1e3;
-        -webkit-border-radius: 50%;
-        -moz-border-radius: 50%;
-        border-radius: 50%;
-    }
-
-    .reply-list:before,
-    .reply-list:after {
-        display: none;
-    }
-
-    .reply-list li:before {
-        content: '';
-        width: 60px;
-        height: 2px;
-        background: #c7cacb;
-        position: absolute;
-        top: 25px;
-        left: -55px;
-    }
-
-
-    .comments-list li {
-        margin-bottom: 15px;
-        display: block;
-        position: relative;
-    }
-
-    .comments-list li:after {
-        content: '';
-        display: block;
-        clear: both;
-        height: 0;
-        width: 0;
-    }
-
-    .reply-list {
-        padding-left: 88px;
-        clear: both;
-        margin-top: 15px;
-    }
-
-    /**
-* Avatar
----------------------------*/
-    .comments-list .comment-avatar {
-        width: 65px;
-        height: 65px;
-        position: relative;
-        z-index: 99;
-        float: left;
-        border: 3px solid #FFF;
-        -webkit-border-radius: 4px;
-        -moz-border-radius: 4px;
-        border-radius: 4px;
-        -webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
-        -moz-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
-        box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
-        overflow: hidden;
-    }
-
-    .comments-list .comment-avatar img {
-        width: 100%;
-        height: 100%;
-    }
-
-    .reply-list .comment-avatar {
-        width: 50px;
-        height: 50px;
-    }
-
-    .comment-main-level:after {
-        content: '';
-        width: 0;
-        height: 0;
-        display: block;
-        clear: both;
-    }
-
-    /**
-* Caja del Comentario
----------------------------*/
-    .comments-list .comment-box {
-        width: 680px;
-        float: right;
-        position: relative;
-        -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
-        -moz-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
-        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
-    }
-
-    .comments-list .comment-box:before,
-    .comments-list .comment-box:after {
-        content: '';
-        height: 0;
-        width: 0;
-        position: absolute;
-        display: block;
-        border-width: 10px 12px 10px 0;
-        border-style: solid;
-        border-color: transparent #FCFCFC;
-        top: 8px;
-        left: -11px;
-    }
-
-    .comments-list .comment-box:before {
-        border-width: 11px 13px 11px 0;
-        border-color: transparent rgba(0, 0, 0, 0.05);
-        left: -12px;
-    }
-
-    .reply-list .comment-box {
-        width: 610px;
-    }
-
-    .comment-box .comment-head {
-        background: #FCFCFC;
-        padding: 10px 12px;
-        border-bottom: 1px solid #E5E5E5;
-        overflow: hidden;
-        -webkit-border-radius: 4px 4px 0 0;
-        -moz-border-radius: 4px 4px 0 0;
-        border-radius: 4px 4px 0 0;
-    }
-
-    .comment-box .comment-head i {
-        float: right;
-        margin-left: 14px;
-        position: relative;
-        top: 2px;
-        color: #A6A6A6;
-        cursor: pointer;
-        -webkit-transition: color 0.3s ease;
-        -o-transition: color 0.3s ease;
-        transition: color 0.3s ease;
-    }
-
-    .comment-box .comment-head i:hover {
-        color: #03658c;
-    }
-
-    .comment-box .comment-name {
-        color: #283035;
-        font-size: 14px;
-        font-weight: 700;
-        float: left;
-        margin-right: 10px;
-    }
-
-    .comment-box .comment-name a {
-        color: #283035;
-    }
-
-    .comment-box .comment-head span {
-        float: left;
-        color: #999;
-        font-size: 13px;
-        position: relative;
-        top: 1px;
-    }
-
-    .comment-box .comment-content {
-        background: #FFF;
-        padding: 12px;
-        font-size: 15px;
-        color: #595959;
-        -webkit-border-radius: 0 0 4px 4px;
-        -moz-border-radius: 0 0 4px 4px;
-        border-radius: 0 0 4px 4px;
-    }
-
-    .comment-box .comment-name.by-author,
-    .comment-box .comment-name.by-author a {
-        color: #03658c;
-    }
-
-    .comment-box .comment-name.by-author:after {
-        content: 'autor';
-        background: #03658c;
-        color: #FFF;
-        font-size: 12px;
-        padding: 3px 5px;
-        font-weight: 700;
-        margin-left: 10px;
-        -webkit-border-radius: 3px;
-        -moz-border-radius: 3px;
-        border-radius: 3px;
-    }
-
-    /** =====================
-* Responsive
-========================*/
-    @media only screen and (max-width: 766px) {
-        .comments-container {
-            width: 480px;
-        }
-
-        .comments-list .comment-box {
-            width: 390px;
-        }
-
-        .reply-list .comment-box {
-            width: 320px;
-        }
-    }
-</style>
 <div class="single-product-area">
     <div class="zigzag-bottom"></div>
     <div class="container">
@@ -504,7 +83,6 @@ $this->breadcrumbs = array(
                                     <p>Your rating</p>
 
                                     <section class='rating-widget'>
-
                                         <!-- Rating Stars Box -->
                                         <div class='rating-stars text-center'>
                                             <ul id='stars'>
@@ -531,9 +109,6 @@ $this->breadcrumbs = array(
                                             <div class='text-message'></div>
                                             <div class='clearfix'></div>
                                         </div>
-
-
-
                                     </section>
 
                                 </div>
@@ -667,7 +242,7 @@ $this->breadcrumbs = array(
 <script src="<?= get_BaseUrl(); ?>/js/jquery2.1.1.min.js"></script>
 <script>
     let value_rating_star = 0;
-
+    $('.success-box').hide();
     /* 1. Visualizing things on Hover - See next part for action on click */
     $('#stars li').on('mouseover', function() {
         var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
@@ -704,9 +279,9 @@ $this->breadcrumbs = array(
         var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
         var msg = "";
         if (ratingValue > 1) {
-            msg = "Thanks! You rated this " + ratingValue + " stars.";
+            msg = "Thanks! You rate this " + ratingValue + " stars.";
         } else {
-            msg = "We will improve ourselves. You rated this " + ratingValue + " stars.";
+            msg = "We will improve ourselves. You rate this " + ratingValue + " stars.";
         }
         responseMessage(msg);
         value_rating_star = ratingValue;
@@ -715,6 +290,7 @@ $this->breadcrumbs = array(
 
 
     function responseMessage(msg) {
+        $('.success-box').show();
         $('.success-box').fadeIn(200);
         $('.success-box div.text-message').html("<span>" + msg + "</span>");
     }
